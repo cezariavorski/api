302c334568cfaeed60e59dc0abcc4079d27777d5
Cezar Iavorski <ceziavorski@gmail.com>
2023-01-27 02:04:27 -0300

    18 - parametro pelo body

--- Program.cs
+++ Program.cs
@@ -3,12 +3,22 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapPost("/", () => new{Name="Cezar Iavorski", age=27});
-app.MapPost("/AddHeader", (HttpResponse response) => {
+app.MapGet("/AddHeader", (HttpResponse response) => {
     response.Headers.Add("Test", "Cezar Iavorski");
     return new {Name="Cezar Iavorski", age=27};
 
-} );
+});
+
+app.MapPost("/saveproduct", (Product product) => {
+    return product.Code + "-" + product.Name;
+});
     
 
 
 app.Run();
+
+
+public class Product{
+    public string Code { get; set; }
+    public string Name { get; set; }
+}
