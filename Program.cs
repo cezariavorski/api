abec186a39311909caffa7842d43af1b3433d686
Cezar Iavorski <ceziavorski@gmail.com>
2023-01-27 01:55:25 -0300

    17 - Trabalhando com endpoits

--- Program.cs
+++ Program.cs
@@ -2,5 +2,13 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+app.MapPost("/", () => new{Name="Cezar Iavorski", age=27});
+app.MapPost("/AddHeader", (HttpResponse response) => {
+    response.Headers.Add("Test", "Cezar Iavorski");
+    return new {Name="Cezar Iavorski", age=27};
+
+} );
+    
+
 
 app.Run();
